bb4d7d74130070728e8974d430166bc0e0b143cf
Davson Santos <davson.santos@ivygroup.tech>
2025-09-17 13:59:20 -0400

    refactor: Remove api.js and netlify.toml; streamline index.js for server initialization

--- api.js
+++
@@ -1,7 +0,0 @@
-console.log("--- EXECUTANDO api.js ---"); // Log de depuração
-require('dotenv').config();
-const serverless = require('serverless-http');
-const app = require('./index'); // Importa seu app Express do index.js
-
-// Envolve o app Express em um manipulador compatível com a Netlify
-module.exports.handler = serverless(app);
