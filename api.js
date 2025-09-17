57880d1139cc0d15256fefa5ca99bc9a734281eb
Davson Santos <davson.santos@ivygroup.tech>
2025-09-17 13:38:59 -0400

    refactor: Improve comments for clarity in api.js and netlify.toml

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const serverless = require('serverless-http');
-const app = require('./index'); // Importa seu app Express
+const app = require('./index'); // Importa seu app Express do index.js
 
 // Envolve o app Express em um manipulador compatível com a Netlify
 module.exports.handler = serverless(app);
